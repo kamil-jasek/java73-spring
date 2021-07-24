709fbcddd7704cf47535e20027fa15612d5703d9
Kamil Jasek <kamil.jasek@dbschenker.com>
2021-07-24 13:45:37 +0200

    customer service tests

--- src/test/java/pl/sda/customers/service/CustomerServiceTest.java
+++ src/test/java/pl/sda/customers/service/CustomerServiceTest.java
@@ -1,12 +1,16 @@
 package pl.sda.customers.service;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.sda.customers.dto.RegisterCompanyForm;
+import pl.sda.customers.entity.Company;
 import pl.sda.customers.entity.CustomerRepository;
+import pl.sda.customers.exception.EmailAlreadyExistsException;
+import pl.sda.customers.exception.VatAlreadyExistsException;
 
 class CustomerServiceTest extends BaseServiceTest {
 
@@ -27,5 +31,25 @@ class CustomerServiceTest extends BaseServiceTest {
         // then
         assertNotNull(customerId);
         assertTrue(repository.existsById(customerId.getId()));
+    }
+
+    @Test
+    void shouldNotRegisterCompanyIfEmailAlreadyExists() {
+        // given
+        persistAndClearCache(new Company("abc@wp.pl", "Compa S.A.", "PL939230203"));
+        final var form = new RegisterCompanyForm("abc@wp.pl", "PL02030303", "Test S.A.");
+
+        // then
+        assertThrows(EmailAlreadyExistsException.class, () -> customerService.registerCompany(form));
+    }
+
+    @Test
+    void shouldNotRegisterCompanyIfVatAlreadyExists() {
+        // given
+        persistAndClearCache(new Company("bca@wp.pl", "Comp S.A.", "PL98393993"));
+        final var form = new RegisterCompanyForm("abc@wp.pl", "PL98393993", "Test S.A.");
+
+        // then
+        assertThrows(VatAlreadyExistsException.class, () -> customerService.registerCompany(form));
     }
 }
