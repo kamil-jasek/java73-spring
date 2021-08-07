cbb1ae37df6d19c7394729ce2ae081ea6e6adda2
Kamil Jasek <kamil.jasek@dbschenker.com>
2021-08-07 13:31:53 +0200

    security config

---
+++ src/main/java/pl/sda/customers/rest/UserSignInRestController.java
@@ -0,0 +1,15 @@
+package pl.sda.customers.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public/user")
+final class UserSignInRestController {
+
+    @GetMapping
+    String test() {
+        return "test";
+    }
+}
